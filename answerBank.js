586d285db0b502ac94489d31688eaab70dbd3c34
Mike <mike.bryant.jr@icloud.com>
2018-07-26 22:19:57 -0600

    first commit

---
+++ answerBank.js
@@ -0,0 +1,133 @@
+const answerBank = [{
+    question: 'Which super hero was created by Marvel?',
+    answers: [
+      'Superman',
+      'Spider-Man',
+      'The Flash',
+      'Firestorm'
+    ],
+    rightAnswer: 'Spider-Man',
+    icon: 'https://static.comicvine.com/uploads/original/11116/111163466/4358456-5490742799-spide.jpg',
+    alt: 'Spider-Man'
+  },
+  {
+    question: 'Which super hero was created by DC?',
+    answers: [
+      'Hawk Eye',
+      'Green Arrow',
+      'Captian America',
+      'Thor'
+    ],
+    rightAnswer: 'Green Arrow',
+    icon: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/Green_Arrow_%28DC_Rebirth%29.jpg/220px-Green_Arrow_%28DC_Rebirth%29.jpg',
+    alt: 'Green Arrow'
+  },
+  {
+    question: '3)Which of these are a DC Villain?',
+    answers: [
+      'Ultron',
+      'Dr. Doom',
+      'Vandal Savage',
+      'Thanos'
+    ],
+    rightAnswer: 'Vandal Savage',
+    icon: 'https://pm1.narvii.com/5746/7c17322559c4ae84ca92bde14aeb70178837bf31_hq.jpg',
+    alt: 'Vandal Savage'
+  },
+  {
+    question: 'Which of these are a Marvel Villain?',
+    answers: [
+      'Zoom',
+      'Loki',
+      'Bane',
+      'The Joker'
+    ],
+    rightAnswer: 'Loki',
+    icon: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Loki_Laufeyson_Earth_616.jpg/250px-Loki_Laufeyson_Earth_616.jpg',
+    alt: 'Loki'
+  },
+  {
+    question: 'Which of these organizations is from Marvel?',
+    answers: [
+      'Star Labs',
+      'Queen Industries',
+      'Mercury Labs',
+      'S.H.I.E.L.D.'
+    ],
+    rightAnswer: 'S.H.I.E.L.D.',
+    icon: 'https://images-mm.s3.amazonaws.com/Avengers_Shield_Logo_Black_Magnet_POP.jpg',
+    alt: 'S.H.I.E.L.D.'
+  },
+  {
+    question: 'Which of the following objects can not be found in the Marvel Universe?',
+    answers: [
+      'Infinity Stones',
+      'Kryptonite',
+      'Terrigan Crystals',
+      'Mjolnir'
+    ],
+    rightAnswer: 'Kryptonite',
+    icon: 'https://www.dccomics.com/sites/default/files/imce/2018/04-APR/Kryptonite_5ac542501b7164.42921287.jpg',
+    alt: 'Kryptonite'
+  },
+  {
+    question: 'Which of these is the identity of a Marvel Character?',
+    answers: [
+      'Clark Kent',
+      'Bruce Wayne',
+      'Steve Rogers',
+      'Eobard Thawne'
+    ],
+    rightAnswer: 'Steve Rogers',
+    icon: 'https://static.comicvine.com/uploads/original/5/52086/1875996-steve_rogers.jpg',
+    alt: 'Steve Rogers'
+  },
+  {
+    question: 'Which of these is the identity of a DC character?',
+    answers: [
+      'Bruce Banner',
+      'Stan Lee',
+      'Tony Stark',
+      'Barry Allen'
+    ],
+    rightAnswer: 'Barry Allen',
+    icon: 'https://pre00.deviantart.net/87a5/th/pre/f/2016/091/6/6/barry_allen__earth_27__commission_by_phil_cho-d9xahva.jpg',
+    alt: 'Barry Allen'
+  },
+  {
+    question: "Which of these Marvel characters don't have a stand alone movie?",
+    answers: [
+      'Deadpool',
+      'Iron Man',
+      'Black Widow',
+      'Hulk '
+    ],
+    rightAnswer: 'Black Widow',
+    icon: 'https://www.screengeek.net/wp-content/uploads/2015/09/black-widow.jpg',
+    alt: 'Black Widow'
+  },
+  {
+    question: "Which of these DC characters don't have a stand alone movie?",
+    answers: [
+      'Batman',
+      'Wonder Woman',
+      'Superman',
+      'Aquaman'
+    ],
+    rightAnswer: 'Aquaman',
+    icon: 'http://pm1.narvii.com/6317/b9e827c80c09555cc47f27cd5e8ba50f569150ff_00.jpg',
+    alt: 'Aquaman'
+  },
+]
+
+//Correct answer image url: http://assets1.ignimgs.com/2017/09/08/deadpool-1504831592157_1280w.jpg
+// or clapping gif https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiq67bIspfcAhUBheAKHUvzDwEQjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fdeadpool-applause-clapping-l0MYDGA3Du1hBR4xG&psig=AOvVaw2ZDqvuTesWkIpypCUWup_p&ust=1531410388310765
+
+//incorrect answer image url: https://www.unilad.co.uk/wp-content/uploads/2018/03/Deadpool-web-thumb.jpg
+// gif url: https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjQ4u_Rs5fcAhVoh-AKHWIgA5UQjRx6BAgBEAU&url=http%3A%2F%2Fmyrpdummies.tumblr.com%2Fpost%2F139796539093%2Fdeadpool-gif-hunt&psig=AOvVaw2ZDqvuTesWkIpypCUWup_p&ust=1531410388310765
+
+//background marvel vs. DC image url:https://vignette.wikia.nocookie.net/47cfae55-5914-400b-ba86-0cf1b5dda8d6/scale-to-width-down/800
+
+// logo urls: http://1000logos.net/wp-content/uploads/2017/08/Marvel-Logo.png
+
+//
---
+++ index.js
@@ -0,0 +1,145 @@
+let numberOfQuestion = 0;
+let yourScore = 0;
+
+//generate question html
+function populateQuestion() {
+  if (numberOfQuestion <= answerBank.length - 1) {
+    return `<div class="questionForm">
+        <h2>${answerBank[numberOfQuestion].question}</h2>
+        <form class="submitNow">
+          <fieldset>
+          <legend class="questionsAndAnswers"></legend>
+            <label class="answerBackground">
+              <input type="radio" name="answers" value="${answerBank[numberOfQuestion].answers[0]}" required>
+              <span>${answerBank[numberOfQuestion].answers[0]}</span>
+            </label>
+            <label class="answerBackground">
+              <input type="radio" name="answers" value="${answerBank[numberOfQuestion].answers[1]}" required>
+              <span>${answerBank[numberOfQuestion].answers[1]}</span>
+            </label>
+            <label class="answerBackground">
+              <input type="radio" name="answers" value="${answerBank[numberOfQuestion].answers[2]}" required>
+              <span>${answerBank[numberOfQuestion].answers[2]}</span>
+            </label>
+            <label class="answerBackground">
+              <input type="radio" name="answers" value="${answerBank[numberOfQuestion].answers[3]}" required>
+              <span>${answerBank[numberOfQuestion].answers[3]}</span>
+            </label>
+            <button onClick="pickAnswer()" type="submit" class="questionButton">Submit</button>
+        </fieldset>
+        </form>
+      </div>`
+  } else {
+    renderResults();
+    restartQuiz();
+    $('.currentQuestion').text(10)
+  }
+}
+
+//change question Number
+function changeQuestionNumber() {
+  numberOfQuestion++;
+  $('.currentQuestion').text(numberOfQuestion + 1);
+}
+
+//change yourScore
+function changeYourScore() {
+  yourScore++;
+}
+
+
+function beginQuiz() {
+  $('.startOfQuiz').on('click', '.quizStartButton', function (event) {
+    $('.startOfQuiz').remove();
+    $('.questionsForm').css('display', 'block');
+    $('.currentQuestion').text(1);
+    createQuestion();
+  });
+}
+
+// render question in DOM
+function createQuestion() {
+  $('.questionsForm').html(populateQuestion());
+
+}
+
+//user selects answer on submit run user feedback
+function pickAnswer() {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    let pickedAnswer = $('input:checked');
+    let currentAnswer = pickedAnswer.val();
+    let rightAnswer = `${answerBank[numberOfQuestion].rightAnswer}`;
+    if (currentAnswer === rightAnswer) {
+      pickedAnswer.parent().addClass('correct');
+      ifRightAnswer();
+    } else {
+      pickedAnswer.parent().addClass('wrong');
+      ifWrongAnswer();
+    }
+  });
+}
+
+function ifRightAnswer() {
+  correctAnswer();
+  addToYourScore();
+}
+
+function ifWrongAnswer() {
+  wrongAnswer();
+}
+
+//user feedback for correct answer
+function correctAnswer() {
+  let rightAnswer = `${answerBank[numberOfQuestion].rightAnswer}`;
+  $('.questionsForm').html(`<div class="answerForm questionForm"><div class="icon"><img src="http://assets1.ignimgs.com/2017/09/08/deadpool-1504831592157_1280w.jpg" alt="Approving Deadpool"/><img src="${answerBank[numberOfQuestion].icon}" alt="${answerBank[numberOfQuestion].alt}"/></div><p><b>Impressive!  You know your stuff!</b></p><button type=button class="nextButton questionButton">Next</button></div>`);
+}
+
+//user feedback for wrong answer
+function wrongAnswer() {
+  let rightAnswer = `${answerBank[numberOfQuestion].rightAnswer}`;
+  $('.questionsForm').html(`<div class="answerForm questionForm"><div class="icon"><img src="https://www.unilad.co.uk/wp-content/uploads/2018/03/Deadpool-web-thumb.jpg" alt="DisApproving Deadpool"/><img src="${answerBank[numberOfQuestion].icon}" alt="${answerBank[numberOfQuestion].alt}"/></div><p><b>You need to freshen up on your comics!</b><br>The correct answer is <span>"${rightAnswer}"</span></p><button type=button class="nextButton questionButton">Next</button></div>`);
+}
+
+//update score text
+function addToYourScore() {
+  changeYourScore();
+  $('.yourScore').text(yourScore);
+}
+
+//when quiz is over this is the html for the page
+function renderResults() {
+  if (yourScore == 10) {
+    $('.questionsForm').html(`<div class="results answerForm questionForm"><img src="https://nostalgiaking.com/wp-content/uploads/2018/02/deadpool-2.jpg" alt="Deadpool animated icon"/><p>You got ${yourScore} / 10</p><p>PERFECTION!!!!</p><button class="restartButton questionButton">Restart Quiz</button></div>`);
+  } else if (yourScore < 10 && yourScore >= 8) {
+    $('.questionsForm').html(`<div class="results answerForm questionForm"><img src="http://assets1.ignimgs.com/2017/09/08/deadpool-1504831592157_1280w.jpg" alt="Deadpool animated icon"/><p>You got ${yourScore} / 10</p><p>You really know your stuff but always continue to read more comics!</p><button class="restartButton questionButton">Restart Quiz</button></div>`);
+  } else if (yourScore < 8 && yourScore >= 5) {
+    $('.questionsForm').html(`<div class="results answerForm questionForm"><h3>Close but no cigar</h3><img src="http://assets1.ignimgs.com/2018/05/16/deadpool2-thumb2-1526510546529_1280w.jpg" alt="Deadpool"/><p>You got ${yourScore} / 10</p><p>Almost but you need to read more comics if you want to call your self a comic know it all!</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else {
+    $('.questionsForm').html(`<div class="results answerForm questionForm"><h3>You haven't even watched the movies</h3><img src="https://www.unilad.co.uk/wp-content/uploads/2018/03/Deadpool-web-thumb.jpg" alt="car animcated icon"/><p>You got ${yourScore} / 10</p><p>You should probably start watching some of the movies if you want to stay up on the times!</p><button class="restartButton">Restart Quiz</button></div>`);
+  }
+}
+
+//next question Button
+function renderNextQuestion() {
+  $('main').on('click', '.nextButton', function (event) {
+    changeQuestionNumber();
+    createQuestion();
+  });
+}
+
+//restart quiz
+function restartQuiz() {
+  $('main').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+}
+
+//run quiz functions
+function startTheQuiz() {
+  beginQuiz();
+  pickAnswer();
+  renderNextQuestion();
+}
+
+$(startTheQuiz);
